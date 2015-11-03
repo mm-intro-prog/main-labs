47837fb286898dc4015eb6294d5f7ad0835bc54b
mbMosman <mbmosman@users.noreply.github.com>
2015-11-02 19:35:38 -0600

    change for python intro lab

---
+++ prog-new/python/welcome.py
@@ -0,0 +1,67 @@
+"""
+This is a comment in python.  We can make comments span multiple lines
+by enclosing our text in triple quotes.
+
+We will also make comments in the code by doing the following:
+"""
+# Hello this is a comment made with the '#', which only works for a single line.
+
+# Enter your name here.
+your_name = ""
+
+
+print( "Welcome to Python " + your_name)
+
+####################################################
+# Exercise 1: Say (Print) a message that says you (by
+#    name) can'twait to learn python.  Use the
+#    your_name variable.
+####################################################
+
+
+
+
+####################################################
+# Exercise 2: Make some variables
+#    - Make a variable called x, and set it to 18
+#    - Make a variable called y, and set it to 24
+#    - Print the result of x minus y
+#    - Make a new variable z, and set it to x plus y
+#    - Print the value of z
+####################################################
+
+
+
+
+####################################################
+# Exercise 3: Do some math!
+#    - Make a variable called sum
+#    - Set it to the sum of any 5 numbers
+####################################################
+
+
+
+
+####################################################
+# Exercise 4: Get parts of a string
+#    - Make a variable called message
+#    - Set it to "Python is an awesome language!"
+#    - Print only characters 8 through 15.
+#    - Before you run it, what should that display?
+####################################################
+
+
+
+####################################################
+# Exercise 5: Make a while loop
+#  Use a while loop to print numbers from 1 to 10
+####################################################
+
+
+
+
+
+####################################################
+# Exercise 6: Make a while loop
+#  Use a while loop to print numbers from 10 to 1
+####################################################
