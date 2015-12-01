b504eaa0152692aa72ae04fae234c98b859ad415
mbMosman <mbmosman@users.noreply.github.com>
2015-11-30 18:45:53 -0600

    lab for oo

---
+++ prog-new/python/objects.py
@@ -0,0 +1,47 @@
+"""
+IMPORTANT STUFF:
+
+This file is basic 'skeleton code' for the exercises that you will be asked to
+complete in this lab.  You must put your answers in the correct section that is
+designated in the comments.  Things should be in order so just scroll down the
+page as you go through the lab exercises.
+
+There is no test code in this lab, so you will always be running the whole file.
+"""
+
+
+your_name = ""
+
+#############################################################
+# Exercise 1
+#
+# Write a class to define a circle.
+# The circle has a single property called radius.
+# The circle has one behavior to calculate the area.
+#   - The area is calculated as follows:
+#        area = 3.14 * radius * radius
+############################################################
+
+
+
+
+#############################################################
+# Exercise 2
+#
+# Use your circle class to make a circle with a radius of 4.
+# Print the radius of the circle.
+# Print the area of the circle.
+############################################################
+
+
+
+
+#############################################################
+# Exercise 3
+#
+# Make a cylinder class that inherits from your cirle.
+# It should add a new property called height.
+# The cylinder has updated behavior to calculate the area.
+#   - The area is calculated as follows:
+#        area = (2 * 3.14 * radius * height) + (3.14 * radius * radius)
+############################################################
