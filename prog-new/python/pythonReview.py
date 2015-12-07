2764ccadd3f5f58697326aca08a61315f11db238
mbMosman <mbmosman@users.noreply.github.com>
2015-12-07 16:57:18 -0600

    updates for final classes

---
+++ prog-new/python/pythonReview.py
@@ -0,0 +1,159 @@
+
+__author__ = 'your_name_here'
+
+
+#########################################################################
+# Problem 1
+#
+# Write a __while__ loop that counts (prints the numbers) from 0 to -10.
+#########################################################################
+
+
+
+##########################################################################
+# Problem 2
+#
+# Write a __for__ loop that counts (print the numbers) from -10 to 0.
+#########################################################################
+
+
+
+#########################################################################
+# Problem 3
+#
+# Write a loop (of any kind) that will count __by 2__ from 1 up to 20.
+#########################################################################
+
+
+
+
+#########################################################################
+# Problem 4
+#
+# Write some code that will prompt the user to enter a number, then
+# print whether that number is less than, greater than, or equal to zero.
+#########################################################################
+
+
+
+
+
+#########################################################################
+# Problem 5
+#
+# Write some code that will:
+# 1. Prompt the user to enter a word.
+# 2. If the word has more than 3 characters, then print the 2nd character.
+# 3. Otherwise, print the message "too short"
+#
+# Testing:
+# If "hi" is entered, you should get "too short".
+# If "bye" is entered, you should get "too short".
+# If "hello" is entered, you should get "e".
+#########################################################################
+
+
+
+
+
+#########################################################################
+# Problem 6
+#
+# 1. Make a list of any 7 numbers, in any order.
+# 2. Print the list.
+# 3. Use a __for in__ loop to print each item in the list one at a time.
+#########################################################################
+
+
+
+
+
+#########################################################################
+# Problem 7
+#
+# 1. Make a list of any 5 words.
+# 2. Print the list.
+# 3. Make a variable called big_word and set it equal to '' (the empty string)
+# 4. Write a loop to add each item from the list onto the end of big_word.
+# 5. Print big_word.
+#
+# If your list contained "hi", "bye", "go", big_word should be "hibyego".
+#########################################################################
+
+
+
+
+
+#########################################################################
+# Problem 8
+#
+# 1. Write a loop to repeatedly ask a user to enter a number until they
+#    enter 'Q' or 'q' to quit.
+# 2. Add each number entered into a list. ('Q' or 'q' should not be put
+#    into the list.)
+# 3. When the user is done entering number, print each item in the list
+#    one at a time.
+#########################################################################
+
+
+
+
+
+#########################################################################
+# Problem 9
+#
+# Write a function that takes two arguments:
+#     - the year a person was born
+#     - the current year
+# It should __return__ back the age of the person.
+#
+# This function does not need to consider the current date
+# or the month or day that a person was born.
+#########################################################################
+
+
+
+
+# Test your function by showing it gets the correct results
+# using the following values:
+#print("When the current year is 1990 and a person was born in 1972, they are 18.")
+print("When the current year is 1990 and a person was born in 1972, they are " + " call your function here ")
+
+#print("When the current year is 2015 and a person was born in 1994, they are 21.")
+print("When the current year is 2015 and a person was born in 1994, they are " + " call your function here ")
+
+#print("When the current year is 1910 and a person was born in 1870, they are 40.")
+print("When the current year is 1910 and a person was born in 1870, they are " + " call your function here ")
+
+
+
+
+############################################################################################################
+# Problem 10
+#
+# When teaching kids division, they often will play a word game where the students will sit in a circle and
+# count up from 1, but with a twist. If a number is divisible by another number, that number is replaced
+# with a word.
+#
+# For example, the rule might be if a number is divisible by two, then they would say woof:
+#
+# 1, woof, 3, woof, 5, woof, 7, woof.
+#
+# The game grows in complexity as more rules are added.  If we add a new rule to say meow when a number
+# is divisible by three, we get:
+#
+# 1, woof, meow, woof, 5, woof meow, 7, woof.
+#
+# Notice that in place of 6 we said both woof and meow because 6 is divisible by both 2 (woof) and 3 (meow).
+#
+# Recall that you can tell if a number is divisible by using the modulus operator (%) if the result is zero
+# (there is no remainder) then the first number is divisible by the second.
+#
+#
+# Your task is to write code to count (print numbers one per line) from 1 to 30 with the following rules:
+#   1. If a number is divisible by 3, then print "beep" instead of the number
+#   2. If a number is divisible by 5, then print "buzz" instead of the number
+#
+# Remember that if the number is divisible by both numbers, both words should print.
+#
+############################################################################################################
