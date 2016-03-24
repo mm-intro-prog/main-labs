58220fac473f30a3eb1fcbe041fd94dfbe88f1b5
mbMosman <mbmosman@users.noreply.github.com>
2016-03-24 09:21:53 -0500

    lab8

---
+++ prog-new/python/more_python.py
@@ -0,0 +1,58 @@
+"""
+This is the firs HW lab for python.
+"""
+
+# Enter your full name here.
+your_name = "Silly Student"
+
+
+print( "HW 8: " + your_name)
+
+####################################################
+# Exercise 1:
+#  Make a while loop to print the even numbers
+#  from 10 to 30.
+####################################################
+
+
+
+
+####################################################
+# Exercise 2:
+#  Use the variable from above called your_name to
+#    - get the length of your name
+#    - Say what letter your name starts with
+#    - Say what letter your name ends with
+# Note if you want to concatenate a string and an int,
+# you must turn the int into a string first by using
+# the str(number) function.
+####################################################
+name_length =
+
+print ("My name is " + str(name_length) + " characters long.")
+
+
+
+####################################################
+# Exercise 3: Letters in name
+#  Use a while loop to print each letter in your
+#  name (use the variable from above).
+####################################################
+
+
+
+
+####################################################
+# Exercise 4: Name backwards
+#  Use a while loop to print each letter in your
+#  name (use the variable from above) backwards.
+####################################################
+
+
+
+
+####################################################
+# Exercise 5: Factorial!
+#  Factorial of 5 is 5 * 4 * 3 * 2 * 1
+#  Use a while loop to calculate and print this value.
+####################################################
